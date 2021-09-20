3ad77e0aa3e87311f722c984a8ea7efe8d7ac390
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-20 16:10:00 -0300

    Cumpre o requisito 1 :rocket:

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 export async function getCategories() {
-  // Implemente aqui
+  const result = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await result.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  return fetch(`'https://api.mercadolibre.com/sites/MLB/'search?category=${categoryId}&q=${query}`)
+    .then((response) => response.json()); 
 }
