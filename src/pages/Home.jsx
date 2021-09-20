86df0577fb31d96185822de698a45237693caf04
Adilson <adilsongb.rabelo@gmail.com>
2021-09-20 17:34:52 -0300

    Requisito 4 concluido

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.requestCategories();
+  }
+
+  async requestCategories() {
+    const response = await getCategories();
+    this.setState({ categories: response });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div>
-        <input type="text" id="" />
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
+      <>
+        <section className="categories">
+          <h4>Categorias</h4>
+          { categories.map(({ name }, i) => (
+            <p key={ i } data-testid="category">{ name }</p>
+          )) }
+        </section>
+        <div>
+          <input type="text" id="" />
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
+      </>
     );
   }
 }
