51a6f59f53bf174509d049a29db5afadd543c01e
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-09-22 15:36:26 -0300

    Adiciona requisito 9

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import CartButton from './CartButton';
 
 export default class ProductDetail extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cart: [],
+    };
+  }
+
+  componentDidMount() {
+    this.loadStorage();
+  }
+
+  addToCart = () => {
+    const { props } = this;
+    const { state } = props.location;
+    const { produto } = state;
+    const { cart } = this.state;
+    this.setState({ cart: [...cart, produto] },
+      () => {
+        const { cart: cartItem } = this.state;
+        localStorage.setItem('cartItens', JSON.stringify(cartItem));
+      });
+  }
+
+  loadStorage = async () => {
+    const storage = localStorage.getItem('cartItens');
+    const parseStorage = await JSON.parse(storage);
+    this.setState({ cart: parseStorage });
+  }
+
   render() {
     const { props } = this;
     const { state } = props.location;
     const { produto } = state;
+    const { cart } = this.state;
     const { id, title, price, thumbnail } = produto;
     return (
       <div>
@@ -16,6 +47,14 @@ export default class ProductDetail extends Component {
         <p>{thumbnail}</p>
         <p>{id}</p>
         <p>{price}</p>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.addToCart }
+        >
+          Adicionar ao Carrinho
+        </button>
+        <CartButton cart={ cart } />
       </div>
     );
   }
