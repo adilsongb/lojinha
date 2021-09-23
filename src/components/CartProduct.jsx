82ecb738406ed77c4c341d09d9b3696cf917d250
Adilson <adilsongb.rabelo@gmail.com>
2021-09-23 11:00:29 -0300

    Requisito 10 concluido

--- src/components/CartProduct.jsx
+++ src/components/CartProduct.jsx
@@ -2,19 +2,89 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class CartProduct extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.checkCounts();
+  }
+
+  handleClick(param) {
+    const { count } = this.state;
+    if (param) this.setState({ count: Number(count) + 1 });
+    else if (count > 0) this.setState({ count: Number(count) - 1 });
+  }
+
+  checkCounts = () => {
+    const { cart, product } = this.props;
+    let amount = 0;
+    cart.forEach((item) => {
+      if (product.id === item.id) amount += 1;
+    });
+    this.setState({ count: amount });
+  }
+
   render() {
-    const { title } = this.props;
+    const {
+      product: { id, title, price, thumbnail },
+      product,
+      decreaseProduct,
+      increaseProduct,
+      removeProduct,
+    } = this.props;
+    const { count } = this.state;
     return (
-      <div>
+      <div style={ { border: '1px solid black' } }>
+        <img src={ thumbnail } alt="foto-produto" />
         <h3 data-testid="shopping-cart-product-name">
           { title }
         </h3>
-        <h4 data-testid="shopping-cart-product-quantity"> 1 </h4>
+        <h5>{ `A unidade: R$ ${price}` }</h5>
+        <button
+          type="button"
+          onClick={ () => removeProduct(id) }
+        >
+          X
+        </button>
+        <button
+          type="button"
+          data-testid="product-decrease-quantity"
+          onClick={ () => {
+            decreaseProduct(id, count);
+            this.handleClick();
+          } }
+        >
+          -
+        </button>
+        <h4 data-testid="shopping-cart-product-quantity">{ count }</h4>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ () => {
+            increaseProduct(product);
+            this.handleClick(true);
+          } }
+        >
+          +
+        </button>
       </div>
     );
   }
 }
 
 CartProduct.propTypes = {
-  title: PropTypes.string.isRequired,
+  product: PropTypes.shape({
+    id: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+  cart: PropTypes.arrayOf(PropTypes.object).isRequired,
+  decreaseProduct: PropTypes.func.isRequired,
+  increaseProduct: PropTypes.func.isRequired,
+  removeProduct: PropTypes.func.isRequired,
 };
